637ef155e6beab32c70f3b46536feecb00e44286
Patrick McElhaney <patrick.mcelhaney@ally.com>
2011-03-08 15:38:48 -0500

    Draw the arrows between houses.

--- framed.js
+++ framed.js
@@ -11,14 +11,22 @@ var drawHouse = function (col, row) {
 	var house = paper.set();
 	house.push( paper.rect(10, 30, 60, 60));
 	house.push( paper.path( ['M40 0', 'L0 30', 'L80 30', 'L40 0'].join("") ) );
-	house.translate(GRID_START_X + col * GRID_WIDTH, 100);
-	
+	house.translate( GRID_START_X + col * GRID_WIDTH, 100 );
+	return house;
 };
+
+var drawArrow = function (col, row) {
+	var arrow = paper.path( ['M0 5', 'L40 5', 'L35 0', 'M35 10', 'L40 5'].join("") ).translate(0, 50);
+	arrow.translate( GRID_START_X + 90 + col * GRID_WIDTH, 100 );
+};
+
+
 	
 var frames = [
 	{
 		next: function () {
 			this.house = drawHouse(0, 0);
+			drawArrow(0, 0);
 		},
 		prev: function () {
 			
@@ -46,6 +54,7 @@ $(function () {
 	$('body').bind('keyup.presentation', function (e) {
 		if(e.which === 32 && i < frames.length) {
 			frames[i++].next();
+			return false;
 		} 
 		
 		if(e.which === 37 && i > 0) {
