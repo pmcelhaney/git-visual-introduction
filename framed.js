fb42d737fbd5d6a72230a2f687cc8b34593e92c8
Patrick McElhaney <patrick.mcelhaney@ally.com>
2011-03-08 16:51:48 -0500

    Added bricks.

--- framed.js
+++ framed.js
@@ -8,8 +8,20 @@ var GRID_START_X = 75;
 var SHAPES = {
 	walls: function () { return paper.rect(10, 30, 60, 60); },
 	roof: function () { return paper.path( ['M40 0', 'L0 30', 'L80 30', 'L40 0'].join("") ); },
-	door: function () { return paper.rect(20, 60, 15, 30).attr('fill', '#ddd'); },
-	siding: function () { return paper.path( ['M10 40', 'L70 40', 'M10 50', 'L70 50', 'M10 60', 'L70 60', 'M10 70', 'L70 70', 'M10 80', 'L70 80'].join("") ).attr('stroke-width', 0.5); }
+	door: function () { return paper.rect(21, 60, 16, 30).attr('fill', '#ddd'); },
+	siding: function () { return paper.path( ['M10 38', 'L70 38', 'M10 46', 'L70 46', 'M10 54', 'L70 54', 'M10 62', 'L70 62', 'M10 70', 'L70 70', 'M10 78', 'L70 78', 'M10 86', 'L70 86'].join("") ).attr('stroke-width', 0.5); },
+	brick: function () { 
+		var x, y, xoffset = 10, path = [];
+		for (y = 30; y <= 78; y+= 8) {
+			xoffset = xoffset === 10 ? 20: 10;
+			for (x = xoffset; x <= 60; x+= 20 ) {
+				path.push('M' + x + ' ' + y + 'L' + x + ' ' + (y + 8));
+			}
+		}
+		
+		return paper.path( path.join("") ).attr('stroke-width', 0.5); 
+		
+	}
 };
 	
 var paper;	
@@ -20,7 +32,7 @@ var drawHouse = function (col, row, features) {
 	$(features).each(function (i, name) {
 		house.push( SHAPES[name]() );
 	});
-	house.translate( GRID_START_X + col * GRID_WIDTH, 100 );
+	house.translate( GRID_START_X + col * GRID_WIDTH + 0.5, 100.5 );
 	return house;
 };
 
@@ -55,11 +67,33 @@ var frames = [
 	{
 		next: function () {
 			this.arrow = drawArrow(2, 0);
-			this.house = drawHouse(2, 1, 'walls roof siding door');
+			this.house = drawHouse(2, 1, 'walls roof door');
 	
 		},
 		prev: function () {
 			
+		}
+	},
+
+	{
+		next: function () {
+			this.arrow = drawArrow(3, 0);
+			this.house = drawHouse(3, 1, 'walls roof siding door');
+
+		},
+		prev: function () {
+
+		}
+	},
+
+	{
+		next: function () {
+			this.arrow = drawArrow(4, 0);
+			this.house = drawHouse(4, 1, 'walls roof siding brick door');
+
+		},
+		prev: function () {
+
 		}
 	}
 ];
