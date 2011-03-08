c5c464732caa2b9684aa8654490535cd6770b21c
Patrick McElhaney <patrick.mcelhaney@ally.com>
2011-03-08 15:05:12 -0500

    Started drawing house.

--- framed.js
+++ framed.js
@@ -1,27 +1,40 @@
 (function () {
 	
+var HOUSE_WIDTH = 80;
+var GRID_WIDTH = 700/5;
+var GRID_START_X = 75;	
+var MASTER_Y = 100;
+	
+var paper;	
+	
 var frames = [
 	{
-		next: function () { 
-			this.h1 = $('body').append('<h1>Hello World</h1>')
+		next: function () {
+			var i = 0;
+			this.walls = paper.rect(GRID_START_X + GRID_WIDTH * i++, MASTER_Y, HOUSE_WIDTH, HOUSE_WIDTH);
+			paper.rect(GRID_START_X + GRID_WIDTH * i++, MASTER_Y, HOUSE_WIDTH, HOUSE_WIDTH);
+			paper.rect(GRID_START_X + GRID_WIDTH * i++, MASTER_Y, HOUSE_WIDTH, HOUSE_WIDTH);
+			paper.rect(GRID_START_X + GRID_WIDTH * i++, MASTER_Y, HOUSE_WIDTH, HOUSE_WIDTH);
+			paper.rect(GRID_START_X + GRID_WIDTH * i++, MASTER_Y, HOUSE_WIDTH, HOUSE_WIDTH);
 		},
 		prev: function () {
-			this.h1.remove();
+			
 		}
-	}, 
+	},
 	
-	{	
+	{
 		next: function () {
-			this.originalH1Text = $('h1').text();
-			$('h1').text('Goodbye World');
-		}, 
+			this.walls = paper.rect(220, 100, 100, 100);
+		},
 		prev: function () {
-			$('h1').text(this.originalH1Text);
+			
 		}
 	}
 ];
 
 $(function () {
+    paper = Raphael("canvas", 800, 600);
+	
 	var i = 0;
 	
 	$('body').bind('keyup.presentation', function (e) {
